91eca767070c06653a82e469b9e3ffd0d0014ca5
Nadezda Tishuk <166507@gmail.com>
2017-01-05 19:59:37 +0300

    Fix controller name

--- source/js/auth.js
+++ source/js/auth.js
@@ -3,9 +3,9 @@
 		* Подключить angular
 		* Подключить модуль
 		* Присвоить тегу html атрибут ng-app="app"
-		* Обернуть форму в div c атрибутом ng-controller="sendFormData as sendFormData"
+		* Обернуть форму в div c атрибутом ng-controller="authCtrl as authCtrl"
 		* Присвоить input атрибут ng-model
-		* Присвоить форме ng-submit="sendFormData.submit(user, formName, '/path-to'), где user - массив из данных input, formName - имя формы, '/path-to' - путь запроса
+		* Присвоить форме ng-submit="authCtrl.submit(user, formName, '/path-to'), где user - массив из данных input, formName - имя формы, '/path-to' - путь запроса
 		* Сформировать div с ответом сервера с атрибутом ng-show="formName.$submitted" - данные в этом блоке будут появляться в случае ошибок в форме при отправке
 		* В вышеуказанном блоке создать дополнительные div с ответами на разные ошибки:
 			- ng-show="formName.inputName.$error.required" - обязательное для заполнения поле, где inputName - input.name
@@ -14,7 +14,7 @@
 		* В пределах контроллера вывести блок с переменной status, куда будет выводится ответ сервера об успехе или ошибке.
 	Демо: http://rainjeck.tmweb.ru/sendFormData.html
 */
-app.controller('sendFormData', ['$scope', '$http', function ($scope, $http) {
+app.controller('authCtrl', ['$scope', '$http', function ($scope, $http) {
 	this.submit = function (inputs, form, url) {
 		if ( form.$valid ) {
 			var data = angular.toJson(inputs);
